ad0574a64ed845afba56f84da17a0bafb1462dbe
dinosauregg666 <dinosauregg666@gmail.com>
2024-11-28 15:47:52 +0800

    deprecated：添加弃用版本、放弃save属性输出HTML，改用PHP输出前端HTML（save不实用，变动HTML会影响到模块展示并会清空设置好的数据，而PHP输出则不会）、引入前端脚本与样式（改为注册模式，不直接引用到前端）、使用@wordpress/components内置的组件、更改组件样式、ob_start将输出的内容存储在内存中，而不是直接输出到浏览器；ob_get_clean获取缓冲区中的内容并清除缓冲区，然后返回这些内容作为函数的输出；

--- cus-block.php
+++ cus-block.php
@@ -7,10 +7,23 @@ if(!defined('ABSPATH')) exit;
 
 class CusBlock {
     function __construct() {
-        add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+        add_action('init', array($this, 'adminAssets'));
     }
     function adminAssets() {
-        wp_enqueue_script('cusblockscript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks'));
+        wp_register_style('cusblockcss', plugin_dir_url(__FILE__) . 'build/index.css');
+        wp_register_script('cusblockscript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
+        register_block_type('my-namespace/my-block', array(
+            'editor_script' => 'cusblockscript',
+            'editor_style' => 'cusblockcss',
+            'render_callback' => array($this, 'theHTML')
+        ));
+    }
+    function theHTML($attributes) {
+        ob_start(); ?>
+
+        <p> <?php echo esc_html($attributes['skyColor'] . $attributes['grassColor']); ?> </p>;
+
+        <?php return ob_get_clean();
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import './index.scss'
+import {TextControl, Flex, FlexBlock, FlexItem, Button, Icon} from "@wordpress/components"
+import React from 'react'
+
 wp.blocks.registerBlockType('my-namespace/my-block', {
     title: 'My Custom Block',
     icon: 'smiley',
@@ -6,30 +10,56 @@ wp.blocks.registerBlockType('my-namespace/my-block', {
         skyColor: {type: 'string'},
         grassColor: {type: 'string'},
     },
-    edit: function(props) {
-        function updateSkyColor(event) {
-            props.setAttributes({skyColor: event.target.value})
-        }
-
-        function updateGrassColor(event) {
-            props.setAttributes({grassColor: event.target.value})
-        }
-
-        return (
-            <div>
-                <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
-                <input type="text" placeholder="grass color" value={props.attributes.grassColor}  onChange={updateGrassColor} />
-            </div>
-        )
-    },
+    edit: EditComponent,
     save: function(props) {
-        return (
-            <div>
-                color:
-                <span className="skyColor">{props.attributes.skyColor}</span>
-                ===========
-                <span className="grassColor">{props.attributes.grassColor}</span>
-            </div>
-        )
-    }
+        return null
+    },
+    deprecated: [{
+        attributes: {
+            skyColor: {type: 'string'},
+            grassColor: {type: 'string'},
+        },
+        save: function(props) {
+            return (
+                <div>
+                    color:
+                    <span className="skyColor">{props.attributes.skyColor}</span>
+                    ===========
+                    <span className="grassColor">{props.attributes.grassColor}</span>
+                </div>
+            )
+        },
+    }]
 });
+
+function EditComponent(props) {
+    function updateSkyColor(event) {
+        props.setAttributes({skyColor: event.target.value})
+    }
+
+    function updateGrassColor(event) {
+        props.setAttributes({grassColor: event.target.value})
+    }
+
+    return (
+        <div className="myClass">
+            <TextControl label="Question:" style={{fontSize: "20px"}} />
+            <p style={{fontSize: "13px", margin: "20px 0 10px 0"}}>Answers:</p>
+            <Flex>
+                <FlexBlock>
+                    <TextControl></TextControl>
+                </FlexBlock>
+                <FlexItem>
+                    <Button>
+                        <Icon className="mark-as-correct" icon="star-empty"></Icon>
+                    </Button>
+                </FlexItem>
+                <FlexItem>
+                    <Button isLink className="delete">Delete</Button>
+                </FlexItem>
+            </Flex>
+
+            <Button isPrimary>Add another answer</Button>
+        </div>
+    )
+}
